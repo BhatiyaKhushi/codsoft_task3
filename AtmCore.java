9c9ff70752388d4eff138514b5f6284a79119f19
Khushi Bhatiya <khushibhatiyar@gmail.com>
2024-09-15 09:53:10 +0530

    Add files via upload

---
+++ AtmCore.java
@@ -0,0 +1,40 @@
+package com.codsoft.atm;
+
+public class AtmCore {
+
+	
+	    private double balance;
+	    private double depositAmount;
+	    private double withdrawAmount;
+
+	    
+	    public AtmCore(){
+
+	    }
+
+	    
+	public double getBalance() {
+	        return balance;
+	    }
+
+	    public void setBalance(double balance) {
+	        this.balance = balance;
+	    }
+
+	    public double getDepositAmount() {
+	        return depositAmount;
+	    }
+
+	    public void setDepositAmount(double depositAmount) {
+	        this.depositAmount = depositAmount;
+	    }
+
+	    public double getWithdrawAmount() {
+	        return withdrawAmount;
+	    }
+
+	    public void setWithdrawAmount(double withdrawAmount) {
+	        this.withdrawAmount = withdrawAmount;
+	    }
+	}
+
