237555241b3f438a095ee426704e70761359aaf1
Khushi Bhatiya <khushibhatiyar@gmail.com>
2024-09-15 09:52:51 +0530

    Add files via upload

---
+++ AtmTransactionHandler.java
@@ -0,0 +1,14 @@
+package com.codsoft.atm;
+
+public interface  AtmTransactionHandler {
+	
+	  public static  void viewBalance() {
+	}
+	  public static  void withdrawAmount(double withdrawAmount) {
+	}
+	  public static  void depositAmount(double depositAmount ) {
+	}
+	  public static   void viewMiniStatement() {
+	}
+
+}
